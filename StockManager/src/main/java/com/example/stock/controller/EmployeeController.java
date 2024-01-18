c8f1adafbecad0dc1fdd3398ef370d39bf277ccf
kevin <kevin@192.168.0.11>
2024-01-18 15:13:49 +0800

    java

---
+++ StockManager/src/main/java/com/example/stock/controller/EmployeeController.java
@@ -0,0 +1,68 @@
+package com.example.stock.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.stock.dto.EmployeeDto;
+import com.example.stock.dto.EmployeePageDto;
+import com.example.stock.entity.Employee;
+import com.example.stock.service.EmployeeService;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeService employeeService;
+	
+	// page=頁數&size=每頁筆數
+	// 例如: page=0&size=5 第 1 頁每頁 5 筆
+	// 例如: page=1&size=5 第 2 頁每頁 5 筆
+	// 例如: page=2&size=5 第 3 頁每頁 5 筆
+	@GetMapping("/")
+	public String index(@RequestParam(name = "page", defaultValue = "0") int page, 
+						@RequestParam(name = "size", defaultValue = "3") int size,
+						@ModelAttribute EmployeeDto employeeDto, Model model) {
+		Pageable pageable = PageRequest.of(page, size);
+		EmployeePageDto employeePageDto = employeeService.findAll(pageable); // 得到該分頁的數據實體
+		model.addAttribute("employeePageDto", employeePageDto);
+		return "employee";
+	}
+	
+	@PostMapping("/")
+	public String create(EmployeeDto employeeDto) {
+		employeeService.add(employeeDto);
+		return "redirect:/employee/";
+	}
+	
+	@GetMapping("/edit/{id}") // 修改頁面的呈現
+	public String edit(@PathVariable("id") Long id, Model model) {
+		EmployeeDto employeeDto = employeeService.getEmployeeById(id);
+		model.addAttribute("employeeDto", employeeDto);
+		return "employee-edit";
+	}
+	
+	@PutMapping("/{id}")
+	public String update(@PathVariable("id") Long id, EmployeeDto employeeDto) {
+		employeeService.update(employeeDto, id);
+		return "redirect:/employee/";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String delete(@PathVariable("id") Long id) {
+		employeeService.delete(id);
+		return "redirect:/employee/";
+	}
+}
